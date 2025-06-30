4757f7b347d0ce4180dea85e34e593012673d0b6
satrio.dev <mail@satrio.dev>
2025-06-30 07:16:55 +0700

    Update favicon and apple-touch-icon references in app.blade.php; replace favicon.ico with favicon.png for consistency. Update binary files for favicon and apple-touch-icon.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -77,7 +77,7 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
+        <link rel="icon" href="/favicon.png" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
